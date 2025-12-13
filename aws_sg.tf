f8c4872dc5909d78ef5973ef3dea998e3258537d
Siva Reddy <venkatasivareddy997@gmail.com>
2025-12-13 14:44:54 +0530

    updated Security group for Nautilus App Servers

--- aws_sg.tf
+++ aws_sg.tf
@@ -1,3 +1,19 @@
-resource "aws_security_group" "devops_sg"{
-    
+resource "aws_security_group" "sg" {
+    name = "nautilus-sg"
+    ingress {
+        from_port= 80
+        to_port=80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    description = "Security group for Nautilus App Servers"
+
 }
